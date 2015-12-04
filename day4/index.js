24ab5d7abd38735e0c7bcc9cdfd665fde2467f9f
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-12-04 13:48:11 +0100

    Improve day 4 solution

--- day4/index.js
+++ day4/index.js
@@ -1,10 +1,11 @@
 var md5 = require('md5');
 var input = require('../input')(4);
 
-function getLowestNumber(secret, numZeroes) {
+function getLowestNumber(secret, numZeros) {
     var num = 1;
+    var target = new Array(numZeros + 1).join('0');
 
-    while(md5(secret + num).slice(0, numZeroes) !== new Array(numZeroes + 1).join('0')) {
+    while(md5(secret + num).slice(0, numZeros) !== target) {
         num++;
     }
 
