1d0d03a46d5c470f63f2558d40785caf780615d5
Jonathan Svärdén <jonathan.svarden@gmail.com>
2015-12-07 19:17:57 +0100

    Day 7 done

--- day7/index.js
+++ day7/index.js
@@ -1,6 +1,24 @@
 var input = require('../input')(7);
+var instructions = [];
 
-var evil = '';
+function sortByLength(a, b) {
+    a = a.split('=')[0];
+    b = b.split('=')[0];
+
+    if(a.length < b.length) {
+        return -1;
+    } else if (a.length > b.length) {
+        return 1;
+    } else if (a.length === b.length) {
+        return a.localeCompare(b);
+    }
+}
+
+function getStringToEval(ops) {
+    return ops.join(';').replace(/if|in|do/g, function(m) {
+        return m + 'x';
+    });
+}
 
 input.split('\r\n').forEach(function(line) {
     var parts =  line.replace('LSHIFT', '<<')
@@ -9,6 +27,17 @@ input.split('\r\n').forEach(function(line) {
                      .replace('AND', '&')
                      .replace('NOT', '~').split('->');
 
-    console.log(parts);
-    evil += parts[0] + '=' + parts[1] + ';';
+    instructions.push(parts[1].trim() + '=' + parts[0].trim() + ';');
 });
+
+instructions.sort(sortByLength);
+instructions.push(instructions.shift());
+
+//Part 1
+var a = eval(getStringToEval(instructions));
+console.log(a);
+
+//Part 2
+instructions[0] = 'b=' + a;
+var b = eval(getStringToEval(instructions));
+console.log(b);
