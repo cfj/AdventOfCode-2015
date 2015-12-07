afa9c980545185dd0eac2806377a426f2a0ba263
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-12-07 17:10:22 +0100

    Eval hack

--- day7/index.js
+++ day7/index.js
@@ -1,1 +1,14 @@
 var input = require('../input')(7);
+
+var evil = '';
+
+input.split('\r\n').forEach(function(line) {
+    var parts =  line.replace('LSHIFT', '<<')
+                     .replace('RSHIFT', '>>')
+                     .replace('OR', '|')
+                     .replace('AND', '&')
+                     .replace('NOT', '~').split('->');
+
+    console.log(parts);
+    evil += parts[0] + '=' + parts[1] + ';';
+});
