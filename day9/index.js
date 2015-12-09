cccc5332c27e9a31d1ee67c85cdafbe60468a743
Jonathan Svärdén <jonathan.svarden@gmail.com>
2015-12-09 20:24:23 +0100

    Day 9 done

--- day9/index.js
+++ day9/index.js
@@ -1,10 +1,8 @@
 var input = require('../input')(9);
-var permute = require('permute');
+var shuffle = require('shuffle-array');
 
 var r = /(\w+) to (\w+) = (\d+)/i;
-
-var cities = {};
-var destinations = [];
+var routes = {};
 
 input.split('\n').forEach(function(route) {
     var s = r.exec(route);
@@ -12,29 +10,32 @@ input.split('\n').forEach(function(route) {
     var destination = s[2];
     var distance = +s[3];
     
-    if(!cities[origin]) {
-        cities[origin] = {};
-    }
-
-    if(!cities[destination]) {
-        cities[destination] = {};
-    }
-
-    cities[origin][destination] = distance;
-    cities[destination][origin] = distance;
-    cities[origin][origin] = 0;
-    cities[destination][destination] = 0;
+    routes[origin] = routes[origin] || {};
+    routes[destination] = routes[destination] || {};
+    routes[origin][destination] = distance;
+    routes[destination][origin] = distance;
 });
 
-console.log(cities);
+console.log(routes);
 
-//447 too high
+var locations = Object.keys(routes);
+var max = 0;
+var min = Number.MAX_VALUE;
 
-/*
-Dublin -> London -> Belfast = 982 //compare routes on this form...
-London -> Dublin -> Belfast = 605
-London -> Belfast -> Dublin = 659
-Dublin -> Belfast -> London = 659
-Belfast -> Dublin -> London = 605
-Belfast -> London -> Dublin = 982
-*/
+for(var i = 0; i < 1000000; i++) {
+    var routeLength = 0;
+    locations = shuffle(locations);
+
+    for(var j = 0; j < locations.length - 1; j++) {
+        var from = locations[j];
+        var to = locations[j + 1];
+
+        routeLength += routes[from][to];
+    }
+
+    max = Math.max(max, routeLength);
+    min = Math.min(min, routeLength);
+}
+
+console.log('Min route found:', min);
+console.log('Max route found:', max);
