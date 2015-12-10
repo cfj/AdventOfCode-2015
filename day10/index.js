52f703cffaceec2f6b9fb74a7e5224d41d68143c
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-12-10 08:57:50 +0100

    Day 10 done

---
+++ day10/index.js
@@ -0,0 +1,38 @@
+var input = require('../input')(10);
+
+function lookAndSay(num) {
+    var result = '';
+    var nums = num.split('');
+    var charCount = 1;
+
+    if(nums.length === 1) {
+        return result = '1' + nums[0];
+    }
+
+    for(var i = 0; i <= nums.length - 1; i++) {
+        if(nums[i] === nums[i + 1]) {
+            charCount++;
+        } else {
+            result += charCount + nums[i];
+            charCount = 1;
+        }
+    }
+
+    return result;
+}
+
+function getIteration(iteration, num) {
+    var result = lookAndSay(num);
+
+    if(iteration > 1) {
+        return getIteration(--iteration, result);
+    }
+
+    return result;
+}
+
+var part1 = getIteration(40, input);
+var part2 = getIteration(50, input);
+
+console.log('Part 1 result:', part1.length);
+console.log('Part 2 result:', part2.length);
