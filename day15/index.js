5fda6032aaebfd0dc86b6056c358c099b472e87f
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-12-16 12:38:21 +0100

    Day 15 done

--- day15/index.js
+++ day15/index.js
@@ -1,62 +1,65 @@
 var input = require('../input')(15);
-var permute = require('permute');
+var amounts = require('./amounts');
 
-var r = /(\w+): (\w+) (-?\d+), (\w+) (-?\d+), (\w+) (-?\d+), (\w+) (-?\d+), (\w+) (-?\d+)/i;
-
+var r = /(\w+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)/i;
 var ingredients = {};
 
 input.split('\n').forEach(function(ingredient) {
     var s = r.exec(ingredient);
 
-    //console.log(s);
-
-    ingredients[s[1]] = {};
-    ingredients[s[1]][s[2]] = +s[3];
-    ingredients[s[1]][s[4]] = +s[5];
-    ingredients[s[1]][s[6]] = +s[7];
-    ingredients[s[1]][s[8]] = +s[9];
-    ingredients[s[1]][s[10]] = +s[11];
-    ingredients[s[1]].amount = 0;
+    ingredients[s[1]] = {
+        capacity: +s[2],
+        durability: +s[3],
+        flavor: +s[4],
+        texture: +s[5],
+        calories: +s[6],
+        amount: 0
+    };
 });
-
-//ingredients['Butterscotch'].amount = 44;
-//ingredients['Cinnamon'].amount = 56;
-
-//console.log(ingredients);
 
 function getCookieTotal(ingredients) {
     var c = 0;
     var d = 0
     var f = 0;
     var t = 0;
+    var cal = 0;
 
     for(var i in ingredients) {
         c += ingredients[i].capacity * ingredients[i].amount;
         d += ingredients[i].durability * ingredients[i].amount;
         f += ingredients[i].flavor * ingredients[i].amount;
         t += ingredients[i].texture * ingredients[i].amount;
+        cal += ingredients[i].calories * ingredients[i].amount;
     }
 
-    //console.log(c, d, f, t);
+    if(c < 0) c = 0;
+    if(d < 0) d = 0;
+    if(f < 0) f = 0;
+    if(t < 0) t = 0;
 
-    return c*d*f*t;
+    if(cal === 500) {
+        return c*d*f*t;
+    }
+
+    return 0;
 }
 
-var total = getCookieTotal(ingredients);
-
-//console.log(total);
-
-var amounts = [2,3,5,7];
-while (permute(amounts)){
+function setAmounts(ingredients, amounts) {
     var index = 0;
 
     for(var i in ingredients) {
         ingredients[i].amount = amounts[index];
         index++;
     }
-
-    index = 0;
-
-    console.log(amounts);
-    console.log(ingredients);
 }
+
+var max = 0;
+var amount;
+
+while(amounts.hasNext()) {
+    amount = amounts.next();
+    setAmounts(ingredients, amount);
+    max = Math.max(max, getCookieTotal(ingredients));
+}
+
+console.log(max);
