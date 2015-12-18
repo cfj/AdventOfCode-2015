348957c46755e69e481b12ba25313cb8def53aac
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-12-18 10:04:52 +0100

    Day 18 done

---
+++ day18/index.js
@@ -0,0 +1,82 @@
+var input = require('../input')(18);
+var grid = [];
+var lightsOn = [];
+
+input.split('\r\n').forEach(function(line, i) {
+    grid[i] = line.split('');
+});
+
+function getLightState(x, y, grid, neighbors) {
+    var neighborsOn = neighbors.filter(function(n) {
+        return n === '#';
+    }).length;
+
+    if(x === 0 && y === 0) return '#';
+    if(x === grid.length - 1 && y === 0) return '#';
+    if(x === grid.length - 1 && y === grid.length - 1) return '#';
+    if(x === 0 && y === grid.length - 1) return '#';
+
+    switch(grid[x][y]) {
+        case '.':
+            if(neighborsOn === 3) {
+                return '#';
+            } else {
+                return '.';
+            }
+            break;
+        case '#':
+            if(neighborsOn === 2 || neighborsOn === 3) {
+                return '#';
+            } else {
+                return '.';
+            }
+            break;
+    }
+}
+
+function getNeighbors(x, y, grid) {
+    var neighbors = [];
+
+    for(var i = x - 1; i <= x + 1; i++) {
+        for(var j = y - 1; j <= y + 1; j++) {
+            if(!(grid[i] === undefined || grid[i][j] === undefined || i === x && y === j)) {
+                neighbors.push(grid[i][j]);
+            }
+        }
+    }
+
+    return neighbors;
+}
+
+function animate(steps, grid) {
+    var size = grid.length;
+    var newGrid = [];
+
+    for(var i = 0; i < size; i++) {
+        newGrid[i] = Array.apply(null, Array(size)).map(Number.prototype.valueOf, 0);
+    }
+
+    if(steps >= 0) {
+        for(var s = 0; s < steps; s++) {
+            for(var i = 0; i < size; i++) {
+                for(var j = 0; j < size; j++) {
+                    newGrid[i][j] = getLightState(i, j, grid, getNeighbors(i, j, grid));
+                }
+            }
+        }
+
+        var totalOn = grid.map(function(row) {
+            return row.filter(function(state) {
+                return state === '#';
+            }).length;
+        }).reduce(function(p, c) {return p + c});
+
+        lightsOn.push(totalOn);
+
+        animate(--steps, newGrid);
+    }
+}
+
+animate(100, grid);
+
+console.log(lightsOn.pop());
